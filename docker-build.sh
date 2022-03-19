71912b30c748f1e10490ea2f92609e29790548ed
Jan Was <jan.was@starburstdata.com>
2022-03-19 18:24:09 +0100

    Remove invalid escapes from docker build script

--- docker-build.sh
+++ docker-build.sh
@@ -4,15 +4,15 @@ set -euo pipefail
 set -x
 
 if [ -f release.properties ]; then
-    VERSION=\$(grep 'project.rel.pl.net.was\\:trino-faker=' release.properties | cut -d'=' -f2)
+    VERSION=$(grep 'project.rel.pl.net.was\\:trino-faker=' release.properties | cut -d'=' -f2)
 else
-    VERSION=\$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout)
+    VERSION=$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout)
 fi
-TAG=nineinchnick/trino-faker:\$VERSION
+TAG=nineinchnick/trino-faker:$VERSION
 
 docker buildx build \
     --platform linux/amd64,linux/arm64 \
     -t "$TAG" \
-    --build-arg VERSION="\$VERSION" \
+    --build-arg VERSION="$VERSION" \
     --push \
     .
